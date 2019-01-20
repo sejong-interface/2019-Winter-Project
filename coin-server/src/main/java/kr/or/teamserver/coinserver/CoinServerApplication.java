07e4fd4aca41dbedf1470c9dfd560d598e853358
kimhanui <cocacola2017@naver.com>
2019-01-20 18:20:15 +0900

    spring initializer 생성(서버팀)

---
+++ coin-server/src/main/java/kr/or/teamserver/coinserver/CoinServerApplication.java
@@ -0,0 +1,14 @@
+package kr.or.teamserver.coinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinServerApplication.class, args);
+	}
+
+}
+
