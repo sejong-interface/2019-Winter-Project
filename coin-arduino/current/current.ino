2609a60eb35ff7aa1444e43615abab3fe0b304a8
Dongmin Kang <riyenas0925@gmail.com>
2019-02-02 12:14:54 +0900

    불필요한 주석 삭제

--- coin-arduino/current/current.ino
+++ coin-arduino/current/current.ino
@@ -14,13 +14,6 @@ void loop()
 {
   double rmsI = coinWasher.calcIrms(1480);
   double rmsPower = rmsI * rmsI;
-
-  /*
-  root["washerld"] = washerId;
-  root["time"] = random(1, 61);
-  root["amountElectric"] = rmsPower;
-  root.printTo(jsonData);
-  */
   
   Serial.println(rmsI); //전류의 RMS(Root Mean Square) 값
   Serial.print(rmsI * rmsV); //피상전력 (Irms * Vrms) 값
