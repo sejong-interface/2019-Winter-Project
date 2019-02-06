90177ec66ca3ff4e2532ec59e3c66ee17b6a5843
2kyung19 <2kyung19@gmail.com>
2019-02-06 14:03:15 +0900

    push-android project

---
+++ push-android/washer/app/src/main/java/kr/or/infe/washer/FirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package kr.or.infe.washer;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("IDService", "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
+
