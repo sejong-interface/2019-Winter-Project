c17511fb70062c7017499451dc7095b95bc97a89
주이식 <helios2sic@gmail.com>
2019-02-10 18:31:54 +0900

    안드로이드 수정본

---
+++ MYAPPL~1 (2) (2)/app/src/main/java/com/example/jkey2/myapplication/model.java
@@ -0,0 +1,44 @@
+package com.example.jkey2.myapplication;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class model {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private int id;
+    private String amountElectric;
+    private String state;
+    private String time;
+
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getAmountElectric(){
+        return amountElectric;
+    }
+
+    public void setAmountElectric(){
+        this.amountElectric = amountElectric;
+    }
+
+    public String getState(){
+        return state;
+    }
+    public void setState(){
+        this.state = state;
+    }
+
+    public String getTime(){
+        return time;
+    }
+    public void setTime(){
+        this.time= time;
+    }
+
+    public String toString(){
+        return this.id +"|" + this.time + "|"+this.state; //
+    }
+}
