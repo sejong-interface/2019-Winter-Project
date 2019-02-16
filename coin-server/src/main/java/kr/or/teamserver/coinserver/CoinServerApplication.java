30fe5b518663fcd2d0245742c9ed3464c7f6bf32
onsae-PC\onsae <vfgtr554@naver.com>
2019-02-16 19:47:08 +0900

    이미지 파일 업로드, 다운로드 기능 구현 -post로 파일을 업로드 가능 -get으로 업로드 된 파일 다운가능
    **파일 업로드 방법
    Postman이용 -> Post로 바꾸고 http://localhost:8080/uploadFile 그 후 Body -> form-data -> 체크박스 체크, KEY에 file 입력, VALUE로 파일 선택 -> 파일 업로드 완료 -> Uploads폴더 자동 생성
    
    **파일 받는 방법
    Postman이용 -> Get으로 바꾸고 http://localhost:8080/downloadFile/파일이름

--- coin-server/src/main/java/kr/or/teamserver/coinserver/CoinServerApplication.java
+++ coin-server/src/main/java/kr/or/teamserver/coinserver/CoinServerApplication.java
@@ -1,9 +1,14 @@
 package kr.or.teamserver.coinserver;
 
+import kr.or.teamserver.coinserver.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class CoinServerApplication {
 
 	public static void main(String[] args) {
---
+++ coin-server/src/main/java/kr/or/teamserver/coinserver/property/FileStorageProperties.java
@@ -0,0 +1,16 @@
+package kr.or.teamserver.coinserver.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
