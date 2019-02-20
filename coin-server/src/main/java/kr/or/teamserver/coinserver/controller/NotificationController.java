d67539431f85cc28c08ae37a50a966007d057986
2kyung19 <2kyung19@gmail.com>
2019-02-21 01:50:40 +0900

    This is a combination of 2 commits. fcm server ( push notifications )

--- coin-server/src/main/java/kr/or/teamserver/coinserver/controller/NotificationController.java
+++ coin-server/src/main/java/kr/or/teamserver/coinserver/controller/NotificationController.java
@@ -1,31 +1,30 @@
 package kr.or.teamserver.coinserver.controller;
 
-import java.time.LocalDate;
-import java.util.*;
+import java.util.Observable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.Calendar;
 
 import kr.or.teamserver.coinserver.service.AndroidPushPeriodicNotifications;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+import org.apache.log4j.Logger;
 
 import kr.or.teamserver.coinserver.service.AndroidPushNotificationsService;
 
 @RestController
 public class NotificationController {
 
+    static Logger logger = Logger.getLogger(NotificationController.class.getName());
+
     @Autowired
     AndroidPushNotificationsService androidPushNotificationsService;
 
     @Scheduled(fixedRate = 5000) //5초 > 실제 서버 동작 시 1000*60*60*24 ( 24시간 )
-    @GetMapping(value = "/send", produces = "application/json")
-    public @ResponseBody ResponseEntity<String> send() throws JSONException {
+    @GetMapping(value = "/send")
+    public @ResponseBody ResponseEntity<String> send() throws JSONException, InterruptedException  {
         String notifications = AndroidPushPeriodicNotifications.PeriodicNotificationJson();
 
         HttpEntity<String> request = new HttpEntity<>(notifications);
@@ -33,13 +32,16 @@ public class NotificationController {
         CompletableFuture<String> pushNotification = androidPushNotificationsService.send(request);
         CompletableFuture.allOf(pushNotification).join();
 
-        try {
+        try{
             String firebaseResponse = pushNotification.get();
             return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        }
+        catch (InterruptedException e){
+            logger.debug("got interrupted!");
+            throw new InterruptedException();
+        }
+        catch (ExecutionException e){
+            logger.debug("execution error!");
         }
 
         return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
