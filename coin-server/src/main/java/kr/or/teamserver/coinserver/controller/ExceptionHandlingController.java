0271f5e8f8f50d33c74bc01a9838c6da5cfb735e
onsae-PC\onsae <vfgtr554@naver.com>
2019-02-26 02:41:19 +0900

    도움말 추가

--- coin-server/src/main/java/kr/or/teamserver/coinserver/controller/ExceptionHandlingController.java
+++ coin-server/src/main/java/kr/or/teamserver/coinserver/controller/ExceptionHandlingController.java
@@ -1,19 +1,23 @@
 package kr.or.teamserver.coinserver.controller;
 
-import kr.or.teamserver.coinserver.exception.FileNotFoundException;
-import kr.or.teamserver.coinserver.exception.FileStorageException;
-import kr.or.teamserver.coinserver.exception.DeviceNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import kr.or.teamserver.coinserver.exception.DeviceNotFound;
+import kr.or.teamserver.coinserver.exception.FileNotFoundException;
 
 @ControllerAdvice
 public class ExceptionHandlingController {
-    @ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Order")  // 404
-    @ExceptionHandler({DeviceNotFound.class, FileNotFoundException.class, FileStorageException.class})
-    public void conflict() {
+    private Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
-        //empty
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such Order")  // 404
+    @ExceptionHandler({ DeviceNotFound.class, FileNotFoundException.class })
+    public String conflict() {
+        logger.error("Exception Error");
+        return "Cannot Found File";
     }
 }
