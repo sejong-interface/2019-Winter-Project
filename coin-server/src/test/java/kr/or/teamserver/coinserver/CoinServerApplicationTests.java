072f06f0454e43355c8d77d325ab4775ed36cd89
kimhanui <cocacola2017@naver.com>
2019-02-27 01:20:32 +0900

    @Enablescheduling 추가, 쓰레기 프로젝트 삭제

--- coin-server/src/test/java/kr/or/teamserver/coinserver/CoinServerApplicationTests.java
+++ coin-server/src/test/java/kr/or/teamserver/coinserver/CoinServerApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CoinServerApplicationTests {
