ae446a93de47a6dc0dae5efca59e5e90c53596e5
kimhanui <cocacola2017@naver.com>
2019-02-27 15:20:56 +0900

    develop에서 잘못 풀&파일정리

--- coin-server/src/main/java/kr/or/teamserver/coinserver/persistence/DeviceDAO.java
+++
@@ -1,9 +0,0 @@
-package kr.or.teamserver.coinserver.persistence;
-
-
-import kr.or.teamserver.coinserver.model.Device;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface DeviceDAO extends CrudRepository<Device, Long> {
-}
